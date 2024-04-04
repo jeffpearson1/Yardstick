e41d51aa75cb094fd1dcb7987f74991acf3a3fb8
SomewhatFabulousSoftware <39279550+SomewhatFabulousSoftware@users.noreply.github.com>
2024-04-04 10:01:58 -0500

    Include some split-out functions

---
+++ Scripts/AdobeDownloader.psm1
@@ -0,0 +1,108 @@
+function Start-AdobeDownload {
+    param(
+        [string[]] $SearchNames
+    )
+    try {
+        $zipfilename = "$($Global:PackageName)_en_US_WIN_64.zip"
+        $ConsoleURL = "https://adminconsole.adobe.com"
+        Write-Log "Tempdir: $Global:tempDir"
+        Write-Log "ID: $Global:id and $id"
+        $Global:Driver = Start-SeDriver -Browser "Firefox" -StartURL $ConsoleURL -DefaultDownloadPath $Global:tempDir\$Global:id Arguments @('--headless')
+        Start-Sleep -Seconds 10
+        # Load the credentials from the password manager
+        $Credentials = Get-StoredCredential -Target "adobe"
+        # Enter the username into the sign-in dialog
+        $Element = Get-SeElement -By ID "EmailPage-EmailField"
+        Invoke-SeKeys -Element $Element -Keys $Credentials.Username
+        $Button = Get-SeElement -By CSSSelector ".spectrum-Button"
+        $Button.click()
+        Start-Sleep -Seconds 10
+        # Sign in via SSO
+        Invoke-SSOAutoSignIn -Target "adminconsole.adobe.com"
+        # Go to the packages tab
+        Write-Log "Selecting Packages tab..."
+        (Get-SeElement -By LinkText "Packages").click()
+        Start-Sleep -Seconds 10
+        # Click the create a package button
+        Write-Log "Clicking 'create a package'"
+        (Get-SeElement -By CSSSelector "button.Dniwja_spectrum-Button:nth-child(2)").click()
+        Start-Sleep -Seconds 5
+        # Named user licensing is default, so just click the next button
+        Write-Log "NU: Click next"
+        (Get-SeElement -By CSSSelector "div.aaz5ma_spectrum-ButtonGroup:nth-child(4) > button:nth-child(2) > span:nth-child(1)").click()
+        Start-Sleep -Seconds 2
+        # Pick managed package
+        Write-Log "Click managed package"
+        (Get-SeElement -By XPath "//*[text()='Managed package']/../following-sibling::*/input").click()
+        Start-Sleep -Seconds 2
+        # Click next
+        Write-Log "Click Next"
+        (Get-SeElement -By CSSSelector "button.Dniwja_spectrum-Button:nth-child(3) > span:nth-child(1)").click()
+        Start-Sleep -Seconds 2
+        # Click the OS dropdown
+        Write-Log "Click OS select dropdown"
+        (Get-SeElement -By XPath "//*[text()='Select platform']").click()
+        Start-Sleep -Seconds 2
+        # Pick Windows (64-bit)
+        Write-Log "Select Windows (64-bit)"
+        (Get-SeElement -By XPath "//*[text()='Windows (64-bit)']").click()
+        Start-Sleep -Seconds 2
+        # Click next
+        Write-Log "Click Next"
+        (Get-SeElement -By CSSSelector "button.Dniwja_spectrum-Button:nth-child(3) > span:nth-child(1)").click()
+        Start-Sleep -Seconds 2
+        # Add application(s) to list
+        Write-Log "Add app to list"
+        foreach ($Name in $SearchNames) {
+            (Get-SeElement -By XPath "//*[text()=`"$Name`"]/../../../following-sibling::*/button[@data-testid='add-product-button']").click()
+            Start-Sleep -Seconds 2
+        }
+        # Click next
+        Write-Log "Click Next"
+        (Get-SeElement -By CSSSelector "button.Dniwja_spectrum-Button:nth-child(3) > span:nth-child(1)").click()
+        Start-Sleep -Seconds 2
+        # Skip Plugins and click next
+        Write-Log "Skip Plugins: Click Next"
+        (Get-SeElement -By CSSSelector "button.Dniwja_spectrum-Button:nth-child(3) > span:nth-child(1)").click()
+        Start-Sleep -Seconds 2
+        # Adobe CC Desktop Options
+        # Disable Auto Update for Computer Labs
+        Write-Log "Disable auto update for computer labs"
+        (Get-SeElement -By XPATH "//*[text()='Enable self-service install']/../../input").click()
+        Start-Sleep -Seconds 2
+        # Click next
+        Write-Log "Click Next"
+        (Get-SeElement -By CSSSelector "button.Dniwja_spectrum-Button:nth-child(3) > span:nth-child(1)").click()
+        Start-Sleep -Seconds 2
+        # Fill out the package name
+        Write-Log "Fill out package name with $Global:PackageName"
+        $PackageName_Field = Get-SeElement -By XPATH "//*/input[@data-testid='package-name-input']"
+        Invoke-SeKeys -Element $PackageName_Field -Keys $Global:PackageName
+        # Click Create package
+        (Get-SeElement -By XPATH "//*/button[@data-testid='cta-button']").click()
+        Write-Log "Package is building and will download automatically..."
+        Write-Log "Filename: $zipfilename"
+        while (!(Test-Path $Global:tempDir\$Global:id\$zipfilename)) {
+        Write-Log "Waiting for download to start..."
+        Start-Sleep -Seconds 5
+        }
+        while ((Get-Item $Global:tempDir\$Global:id\$zipfilename).Length -le 0) {
+        Write-Log "Waiting for download to finish..."
+        Start-Sleep -Seconds 30
+        }
+        Write-Log "Download complete!"
+        # Close Firefox
+        $Driver.Close()
+        # Extract the zip file
+        Write-Log "Expanding downloaded archive..."
+        Expand-Archive $Global:tempDir\$Global:id\$zipfilename -DestinationPath $Global:tempDir\$Global:id
+        # Move into buildspace
+        Write-Log "Moving contents into buildspace..."
+        Move-Item $Global:tempDir\$Global:id\$Global:packageName\Build\* $Global:buildspace\$Global:id\$Global:version -Force
+    }
+    catch {
+        Write-Error "ERROR: There was an issue running Start-AdobeDownload for application $SearchName"
+        return 1
+    }
+    
+}
---
+++ Scripts/YardstickSupport.psm1
@@ -0,0 +1,195 @@
+# Useful Functions
+
+# Write-Log
+# Prints both to the console and also to the log
+# Param: [String] $ContentToWriteToLog, [Switch] $Init (Erase log and print big timestamp for new script run)
+function Write-Log {
+    param(
+        [String]$Content,
+        [Switch]$Init
+    )
+    Push-Location $LOG_LOCATION
+    if($Init) {
+        if (Test-Path $LOG_LOCATION\$LOG_FILE) {
+            Remove-Item $LOG_LOCATION\$LOG_FILE -Force
+        }
+        Write-Output "#######################################################" | Out-File $LOG_FILE
+        Write-Output "LOGGING STARTED AT $(Get-Date -Format "MM/dd/yyyy HH:mm:ss")" | Out-File $LOG_FILE -Append
+        Write-Output "#######################################################" | Out-File $LOG_FILE -Append
+    }
+    if ($Content) {
+        $Content = "$(Get-Date -Format "MM/dd/yyyy HH:mm:ss") - $Content"
+        Write-Output $Content | Out-File $LOG_FILE -Append
+        Write-Output $Content
+    }
+    Pop-Location
+}
+
+
+# ArrayToString
+# Converts an array to a string
+function ArrayToString() {
+    param (
+        [Array] $array
+    )
+    $arrayString = "@("
+    foreach ($value in $array) {
+        $arrayString = "$($arrayString)$([String]$value),"
+    }
+    $arrayString = $arrayString.TrimEnd(",")
+    $arrayString = "$arrayString)"
+    return [String]$arrayString
+}
+
+# Get-RedirectedUrl
+# Follows redirects and returns the actual URL of a resource
+# Param: [String] URL
+# Return: [String] The last URL that is linked to that does not redirect
+function Get-RedirectedUrl() {
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+
+    If ($response.StatusCode -eq "Found")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
+
+# Get-MsiProductCode
+# Returns the product code of an MSI file in the current directory only
+# Param: [String] $filePath
+# Return: [String] The MSI Product Code
+function Get-MsiProductCode() {
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$filePath
+    )
+    # Read property from MSI database
+    $windowsInstallerObject = New-Object -ComObject WindowsInstaller.Installer
+    $msiDatabase = $windowsInstallerObject.GetType().InvokeMember('OpenDatabase', 'InvokeMethod', $null, $windowsInstallerObject, @($filePath, 0))
+    $query = "SELECT Value FROM Property WHERE Property = 'ProductCode'"
+    $view = $msiDatabase.GetType().InvokeMember('OpenView', 'InvokeMethod', $null, $msiDatabase, ($query))
+    $view.GetType().InvokeMember('Execute', 'InvokeMethod', $null, $view, $null)
+    $record = $view.GetType().InvokeMember('Fetch', 'InvokeMethod', $null, $view, $null)
+    $value = $record.GetType().InvokeMember('StringData', 'GetProperty', $null, $record, 1)
+    $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($windowsInstallerObject) 
+    return [String]$value
+}
+
+# Connect-AutoMSIntuneGraph
+# Automatically connects to the Microsoft Graph API using the Intune module
+# Param: None
+# Returns: None
+function Connect-AutoMSIntuneGraph() {
+    # Check if the current token is invalid
+    if (-not $Global:Token) {
+        Write-Log "Getting an Intune Graph Client APItoken..."
+        $Global:Token = Connect-MSIntuneGraph -TenantID $TenantId -ClientID $ClientId -ClientSecret $clientSecret
+    }
+    elseif ($Global:Token.ExpiresOn.ToLocalTime() -lt (Get-Date)) {
+        # If not, get a new token
+        Write-Log "Token is expired. Refreshing token..."
+        $Global:Token = Connect-MSIntuneGraph -TenantID $TenantId -ClientID $ClientId -ClientSecret $clientSecret
+        Write-Log "Token refreshed. New Token Expires at: $($Global:Token.ExpiresOn.ToLocalTime())"
+    }
+    elseif ($Global:Token.ExpiresOn.AddMinutes(-30).ToLocalTime() -lt (Get-Date)) {
+        # For whatever reason, this API stops working 10 minutes before a token refresh
+        # Set at 30 minutes in case we are uploading large files. 
+        Write-Log "Token expires soon - Refreshing token..."
+        # Required to force a refresh
+        Clear-MsalTokenCache
+        $Global:Token = Connect-MSIntuneGraph -TenantID $TenantId -ClientID $ClientId -ClientSecret $clientSecret
+        Write-Log "Token refreshed. New Token Expires at: $($Global:Token.ExpiresOn.ToLocalTime())"
+    }
+    else {
+        Write-Log "Token is still valid. Skipping token refresh."
+    } 
+}
+
+# MOVE-ASSIGNMENTS
+# Gets all assignments for an application and adds them to a target (To)
+# Removes successfully moved assignments from the (From) application
+# Param: [Application]$From, [Application]$To
+# Return: None
+function Move-Assignments {
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [System.Object] $From,
+        [Parameter(Mandatory, Position=1)]
+        [System.Object] $To
+    )
+    $FromAssignments = Get-IntuneWin32AppAssignment -Id $From.id
+    $FromAvailable = ($FromAssignments | Where-Object intent -eq "available").groupId
+    $FromRequired = ($FromAssignments | Where-Object intent -eq "required").groupId
+    if ($FromAvailable) {
+        foreach ($groupId in $FromAvailable) {
+            $maxRetries = 3
+            $try = 0
+            $successfullyAdded = $false
+            while (!$successfullyAdded -and ($try -lt $maxRetries)) {
+                Add-IntuneWin32AppAssignmentGroup -Include -ID $To.id -GroupID $groupId -Intent "available" -Notification "hideAll" | Out-Null
+                # Check that it worked
+                $AssignedGroups = Get-IntuneWin32AppAssignment -ID $To.id
+                if($AssignedGroups | Where-Object GroupID -eq $groupID) {
+                    $successfullyAdded = $true
+                    Write-Log "$GroupID (Available) added successfully to $($To.id)"
+                    Write-Log "Removing $groupID available assignment from $($From.id)"
+                    Remove-IntuneWin32AppAssignmentGroup -ID $From.id -GroupID $groupId | Out-Null
+                }
+                else {
+                    # Wait 1 second and retry
+                    Start-Sleep -Seconds 1
+                }
+            }
+            
+            if (!$successfullyAdded) {
+                Write-Log "ERROR: Cannot add available app assignment to $($To.id) for group $groupID. Will not remove original assignment."
+            }
+        }
+        
+    }
+    if ($FromRequired) {
+        foreach ($groupId in $FromRequired) {
+            $maxRetries = 3
+            $try = 0
+            $successfullyAdded = $false
+            while (!$successfullyAdded -and ($try -lt $maxRetries)) {
+                Add-IntuneWin32AppAssignmentGroup -Include -ID $To.id -GroupID $groupId -Intent "available" -Notification "hideAll" | Out-Null
+                # Check that it worked
+                $AssignedGroups = Get-IntuneWin32AppAssignment -ID $To.id
+                if($AssignedGroups | Where-Object GroupID -eq $groupID) {
+                    $successfullyAdded = $true
+                    Write-Log "$GroupID (Required) added successfully to $($To.id)"
+                    Write-Log "Removing $groupID required assignment from $($From.id)"
+                    Remove-IntuneWin32AppAssignmentGroup -ID $From.id -GroupID $groupId | Out-Null
+                }
+                else {
+                    # Wait 1 second and retry
+                    Start-Sleep -Seconds 1
+                }
+            }
+            
+            if (!$successfullyAdded) {
+                Write-Log "ERROR: Cannot add required app assignment to $($To.id) for group $groupID. Will not remove original assignment."
+            }
+        }
+    }
+}
+
+
+# Get-SameAppAllVersions
+# Returns all versions of an app
+# Accounts for edge cases where an application name might be similar to others, i.e. Mozilla Firefox vs. Mozilla Firefox ESR
+# Param: [String] DisplayName
+# Return: @(PSCustomObject) 
+function Get-SameAppAllVersions($DisplayName) {
+    $AllSimilarApps = Get-IntuneWin32App -DisplayName "$DisplayName*"
+    return ($AllSimilarApps | Where-Object {($_.DisplayName -eq $DisplayName) -or ($_.DisplayName -like "$DisplayName (*")})
+}
