2a5ba836c03ccbcd20bf3a80500480aa7e435823
SomewhatFabulousSoftware <39279550+SomewhatFabulousSoftware@users.noreply.github.com>
2025-03-11 15:57:46 -0500

    Support added for Github Repos

--- Modules/Custom/GithubDownloader.psm1
+++ Modules/Custom/GithubDownloader.psm1
@@ -2,7 +2,7 @@ class GithubDownloader {
     [string]$URL
     [string]$DownloaderRegex
     [string]$LatestVersion
-    [string]$DownloadURL
+    [string]$Filename
 
     GithubDownloader() { 
         $this.Init(@{}) 
@@ -18,10 +18,11 @@ class GithubDownloader {
     }
 
     [void] Update() {
-        $releaseEndpoint = "https://api.github.com/repos/$($this.URL -replace "https://github.com/|/releases")/releases"
+        $releaseEndpoint = "https://api.github.com/repos/$(($this.URL -replace "https://github.com/|/releases").trimEnd("/"))/releases"
         $releaseResponse = Invoke-RestMethod $releaseEndpoint
         $releaseLatest = ($releaseResponse | Sort-Object published_at -Descending)[0]
         $this.LatestVersion = $releaseLatest.tag_name
-        $this.DownloadURL = ($releaseLatest[0].assets | Where-Object browser_download_url -match $this.DownloaderRegex).browser_download_url
+        $this.URL = ($releaseLatest[0].assets | Where-Object browser_download_url -match $this.DownloaderRegex).browser_download_url
+        $this.Filename = ($this.URL -split "/")[-1]
     }
 }
