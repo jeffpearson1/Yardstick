0cda4673a17be93216e34b7e582dbebb6be8d22b
SomewhatFabulousSoftware <39279550+SomewhatFabulousSoftware@users.noreply.github.com>
2025-03-26 09:39:12 -0500

    Added exception handling for credential retrieval

--- Modules/Custom/Invoke-SSOAutoSignIn.psm1
+++ Modules/Custom/Invoke-SSOAutoSignIn.psm1
@@ -7,9 +7,15 @@ function Invoke-SSOAutoSignIn {
     $Username_Field = Get-SeElement -By ID "username"
     $Password_Field = Get-SeElement -By ID "password"
     $Button = Get-SeElement -By CSSSelector ".btn"
-    $Credentials = Get-StoredCredential -Target "cred1"
-    Invoke-SeKeys -Element $Username_Field -Keys $Credentials.username
-    Invoke-SeKeys -Element $Password_Field -Keys $(ConvertFrom-SecureString $Credentials.Password -AsPlainText)
+    try {
+        $Credentials = Get-StoredCredential -Target "cred1"
+        Invoke-SeKeys -Element $Username_Field -Keys $Credentials.username
+        Invoke-SeKeys -Element $Password_Field -Keys $(ConvertFrom-SecureString $Credentials.Password -AsPlainText)
+    }
+    catch {
+        Write-Log "An error occurred while retrieving the stored credentials from credential manager"
+        return $false
+    }
     $Button.click()
     $TrustButton = Get-SeElement -By ID "trust-browser-button" -Timeout 300
     if ($TrustButton) {
