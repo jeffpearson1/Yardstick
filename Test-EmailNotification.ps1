16d3cc8f0f3bf59b68200e92cc1d01b5f0b7e538
SomewhatFabulousSoftware <39279550+SomewhatFabulousSoftware@users.noreply.github.com>
2026-01-13 09:19:50 -0600

    Updated Email Test

--- Test-EmailNotification.ps1
+++ Test-EmailNotification.ps1
@@ -41,8 +41,16 @@ try {
         Initialize-ApplicationTracker
         
         # Add some test data
-        Add-SuccessfulApplication -ApplicationId "TestApp1" -DisplayName "Adobe Acrobat Reader DC" -Version "2024.001.20643" -Action "Updated"
-        Add-SuccessfulApplication -ApplicationId "TestApp2" -DisplayName "Google Chrome" -Version "120.0.6099.216" -Action "Force Updated"
+        $acrobatDependents = @{
+            "Visual C++ Runtime" = "Updated"
+            "Fonts Package"      = "Skipped"
+        }
+        $chromeDependents = @{
+            "Google Update Helper" = "Failed"
+            "PowerShell Prereq"    = "Added"
+        }
+        Add-SuccessfulApplication -ApplicationId "TestApp1" -DisplayName "Adobe Acrobat Reader DC" -Version "2024.001.20643" -Action "Updated" -Dependents $acrobatDependents
+        Add-SuccessfulApplication -ApplicationId "TestApp2" -DisplayName "Google Chrome" -Version "120.0.6099.216" -Action "Force Updated" -Dependents $chromeDependents
         Add-FailedApplication -ApplicationId "TestApp3" -DisplayName "Failed Application" -Version "1.0.0" -ErrorMessage "Download failed - HTTP 404" -FailureStage "Download"
         Add-FailedApplication -ApplicationId "TestApp4" -DisplayName "Another Failed App" -Version "2.0.0" -ErrorMessage "Upload to Intune failed" -FailureStage "Upload"
         
